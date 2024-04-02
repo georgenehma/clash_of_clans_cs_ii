6f5eae7df817ed713c1beb78e8e668dfd9d91256
Saif Sabry <saif-sabry@aucegypt.edu>
2024-04-02 22:46:45 +0200

    Added implementation for the fence class

--- fence.cpp
+++ fence.cpp
@@ -1,6 +1,22 @@
 #include "fence.h"
 
 fence::fence() {
-    setPixmap(QPixmap("").scaled(100,100));
-
+    health = 100;
+    setPixmap(QPixmap(":/fence.webp").scaled(100,100)); // added constructor for fence, including the health and picture
 }
+double fence::getHealth()const{
+    return health;
+}//getter function for fence health
+bool fence::isDestroyed()const{
+    if(health==0){
+        return true;
+    }
+    return false;
+}// returns true if the fence health is zero, meaning the fence is destroyed
+void fence::Damaged(int amount){
+    health-=amount;
+    if(isDestroyed()){
+        delete this;
+    }// function for applying damage to fence, decreasing health of fence with every damage
+}
+
--- fence.h
+++ fence.h
@@ -4,9 +4,13 @@
 #include <QGraphicsItem>
 class fence: public QObject, public QGraphicsPixmapItem
 {
-public:
+private:
     double health; //health per fence
+public:
     fence();
+    double getHealth()const;//getter for health
+    void Damaged(int amount);// function for applying damage to fence, decreasing health of fence
+    bool isDestroyed()const;// boolean function to check if the fence is destroyed or not
 };
 
 #endif // FENCE_H
